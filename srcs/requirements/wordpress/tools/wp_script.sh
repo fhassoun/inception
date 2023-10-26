f7cd6ba25816a75d24152730f1b268317efb0bfe
fhassoun <fhassoun@student.42berlin.de>
2023-10-26 11:27:39 +0200

    next22

--- srcs/requirements/wordpress/tools/wp_script.sh
+++ srcs/requirements/wordpress/tools/wp_script.sh
@@ -6,15 +6,15 @@ then
 else
 	wget http://wordpress.org/latest.tar.gz
 	tar xfz latest.tar.gz
-	mv wordpress/* .
+	# mv wordpress/* .
 	rm -rf latest.tar.gz
-	rm -rf wordpress
+	# rm -rf wordpress
 
 	# sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
 	# sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
 	# sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
 	# sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
-	cp wp-config-sample.php wp-config.php
+	# cp /wordpress/wp-config-sample.php wordpress/wp-config.php
 
 
 fi
