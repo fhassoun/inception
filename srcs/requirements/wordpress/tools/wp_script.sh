d6437462a660151747128ef66e1849a2ae633130
fhassoun <fhassoun@student.42berlin.de>
2023-10-26 13:28:12 +0200

    next27

--- srcs/requirements/wordpress/tools/wp_script.sh
+++ srcs/requirements/wordpress/tools/wp_script.sh
@@ -22,4 +22,12 @@ fi
 
 
 /usr/sbin/php-fpm7.3 -F
+
+curl "http://$WP_DOMAIN/wp-admin/install.php?step=2" \
+  --data-urlencode "weblog_title=$WP_DOMAIN"\
+  --data-urlencode "user_name=$WP_ADMIN_USERNAME" \
+  --data-urlencode "admin_email=$WP_ADMIN_EMAIL" \
+  --data-urlencode "admin_password=$WP_ADMIN_PASSWORD" \
+  --data-urlencode "admin_password2=$WP_ADMIN_PASSWORD" \
+  --data-urlencode "pw_weak=1"
 exec "$@"
