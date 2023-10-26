ac148ba469ffa744caefc0f5fa9b393c424a233c
fhassoun <fhassoun@student.42berlin.de>
2023-10-26 11:14:56 +0200

    next20

--- srcs/requirements/wordpress/tools/wp_script.sh
+++ srcs/requirements/wordpress/tools/wp_script.sh
@@ -10,20 +10,26 @@ else
 	rm -rf latest.tar.gz
 	rm -rf wordpress
 
-	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
-	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
+	# sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
+	# sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
+	# sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
+	# sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
 	cp wp-config-sample.php wp-config.php
 
 
 fi
 cd /var/www/wordpress
 
+wp core config	--dbhost=$MYSQL_HOSTNAME \
+				--dbname=$MYSQL_DATABASE \
+				--dbuser=$MYSQL_USER \
+				--dbpass=$MYSQL_PASSWORD \
+				--allow-root
+
 wp core install --title=$WP_TITLE \
 				--admin_user=$WP_ADMIN_USER \
 				--admin_password=$WP_ADMIN_PASSWORD \
-				--admin_email=$WP_ADMIN_MAIL \
+				--admin_email=$WP_ADMIN_EMAIL \
 				--url=$WP_URL \
 				--allow-root
 
