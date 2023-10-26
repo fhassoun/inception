92d4fa73b8aebde022af039568bfc5b01cdf63e7
fhassoun <fhassoun@student.42berlin.de>
2023-10-26 11:04:42 +0200

    next19

--- srcs/requirements/wordpress/tools/wp_script.sh
+++ srcs/requirements/wordpress/tools/wp_script.sh
@@ -18,5 +18,16 @@ else
 
 
 fi
+cd /var/www/wordpress
 
+wp core install --title=$WP_TITLE \
+				--admin_user=$WP_ADMIN_USER \
+				--admin_password=$WP_ADMIN_PASSWORD \
+				--admin_email=$WP_ADMIN_MAIL \
+				--url=$WP_URL \
+				--allow-root
+
+wp user create $WP_USER $WP_USER_MAIL --role=author --user_pass=$WP_USER_PASSWORD --allow-root
+cd -
+/usr/sbin/php-fpm7.3 -F
 exec "$@"
