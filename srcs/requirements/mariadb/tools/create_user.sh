6cade5b5c5379bcb88c5cf9a0520a21b7dce7751
fhassoun <fhassoun@student.42berlin.de>
2023-10-26 08:33:04 +0200

    changed dockerfile for db2

--- srcs/requirements/mariadb/tools/create_user.sh
+++ srcs/requirements/mariadb/tools/create_user.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 mysql -u root -e "CREATE DATABASE ${MYSQL_DATABASE};"
 mysql -u root -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 mysql -u root -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
