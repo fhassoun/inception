cd1847289a00b384cb3ea2e8653f223e9a8458a2
fhassoun <fhassoun@student.42berlin.de>
2023-10-26 09:42:59 +0200

    next12

--- srcs/requirements/wordpress/tools/wp_script.sh
+++ srcs/requirements/wordpress/tools/wp_script.sh
@@ -10,7 +10,7 @@ else
 	rm -rf latest.tar.gz
 	rm -rf wordpress
 
-	sed -i "s/username_here/$MYSQL_HOSTNAME/g" wp-config-sample.php
+	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
 	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
 	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
 	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
