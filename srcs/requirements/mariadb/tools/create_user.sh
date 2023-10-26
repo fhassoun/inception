1f6ec0ccadf99ad76c2f48f4fc0f459b262d70a2
fhassoun <fhassoun@student.42berlin.de>
2023-10-26 09:50:33 +0200

    next13

--- srcs/requirements/mariadb/tools/create_user.sh
+++ srcs/requirements/mariadb/tools/create_user.sh
@@ -2,7 +2,7 @@
 
 service mysql start 
 
-mysql -u root -e "CREATE DATABASE '${MYSQL_DATABASE}';"
+mysql -u root -e "CREATE DATABASE ${MYSQL_DATABASE};"
 mysql -u root -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 mysql -u root -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
 mysql -u root -e "FLUSH PRIVILEGES;"
